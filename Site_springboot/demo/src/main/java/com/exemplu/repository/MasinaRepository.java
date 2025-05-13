23bdcc37fcd9d54dd58a3621e7ab1c5cff104356
gaby8-bit <viorel.pichler@student.upt.ro>
2025-05-13 12:08:35 +0300

    Delete Site_springboot/demo directory

--- Site_springboot/demo/src/main/java/com/exemplu/repository/MasinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplu.repository;
-
-import com.exemplu.entity.Masina;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MasinaRepository extends JpaRepository<Masina, Integer> {
-
-}
-
